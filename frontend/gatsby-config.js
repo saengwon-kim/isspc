1d7bd72ca5cfef86e4feb027443cb8f2614b1b7a
Saengwon Kim <saengwon.kim@gmail.com>
2022-10-25 16:02:13 +0900

    update icons

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: '바스티유제빵소',
+    siteTitle: '바스티유제빵소',
+    siteHeadline: 'SPC, 먹어서 응원하자!',
     siteTitleAlt: `Bastille Bakery`,
     siteUrl: `https://isspc.pages.dev`,
     siteLanguage: `ko`,
     author: `Saengwon Kim`,
-    description: '바코드만 찍으면 SPC의 손길이 닿은 제품인지 알 수 있는 페이지입니다!',
+    siteDescription: '바코드만 찍으면 SPC의 손길이 닿은 제품인지 알 수 있는 페이지입니다!',
     url: 'https://isspc.pages.dev',
-    image: '/isspc-logo.png'
+    siteImage: '/icons.png'
   },
   plugins: [
     {
