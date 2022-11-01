5bcb01cd1c905008b63694d2c0aa8f7bdf1469dc
Saengwon Kim <saengwon.kim@gmail.com>
2022-11-01 19:52:22 +0900

    change db format

--- frontend/src/@lekoarts/gatsby-theme-cara/libs/static_db.js
+++ frontend/src/@lekoarts/gatsby-theme-cara/libs/static_db.js
@@ -2,35 +2,31 @@ import allow from "../../../../static/db/allow.json"
 import block from "../../../../static/db/block.json"
 
 async function gatherResponse(allow, block, barcode) {
-    const allow_company = allow.company;
-    const allow_product = allow.product;
-    const block_company = block.company;
-    const block_product = block.product;
     var res = {};
     var barcode_company = barcode;
     if (barcode.length > 6) {
         for (let i = 7; i < 10; i++) {
             barcode_company = barcode.slice(0, i);
-            if (barcode_company in allow_company) {
+            if (barcode_company in allow) {
                 res["type"] = "company";
-                res["content"] = allow_company[barcode_company];
+                res["content"] = allow[barcode_company];
                 res["isSPC"] = true;
             }
-            if (barcode_company in block_company) {
+            if (barcode_company in block) {
                 res["type"] = "company";
-                res["content"] = block_company[barcode_company];
+                res["content"] = block[barcode_company];
                 res["isSPC"] = false;
             }
         }
     }
-    if (barcode in allow_product) {
+    if (barcode in allow) {
         res["type"] = "product";
-        res["content"] = allow_product[barcode];
+        res["content"] = allow[barcode];
         res["isSPC"] = true;
     }
-    if (barcode in block_product) {
+    if (barcode in block) {
         res["type"] = "product";
-        res["content"] = block_product[barcode];
+        res["content"] = block[barcode];
         res["isSPC"] = false;
     }
     return JSON.stringify(res);
