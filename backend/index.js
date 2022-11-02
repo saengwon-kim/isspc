d4e6fbc479451a5b394f1ba513da3b580ffe438e
Saengwon Kim <saengwon.kim@gmail.com>
2022-11-02 13:47:03 +0900

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,27 @@
-import allow from "./data/allow.json"
-import block from "./data/block.json"
+import db001 from "./db/db001.json"
 
-async function gatherResponse(allow, block, barcode) {
-    let barcode_company = barcode.slice(0, 7);
-    const allow_company = allow.company;
-    const allow_product = allow.product;
-    const block_company = block.company;
-    const block_product = block.product;
-    var res = {};
-    if (barcode_company in allow_company) {
-        res["type"] = "company";
-        res["content"] = allow_company[barcode_company];
-    }
-    if (barcode in allow_product) {
-        res["type"] = "product";
-        res["content"] = allow_product[barcode];
-    }
-    if (barcode_company in block_company || barcode in block_product) {
-        res = {};
-    }
-    return JSON.stringify(res);
+var db_list = [db001]
+
+async function gatherResponse(barcode) {
+    var res = [];
+    const barcode_query = [
+        barcode.slice(0, 7),
+        barcode.slice(0, 9),
+        barcode
+    ];
+    db_list.forEach((db) => {
+        barcode_query
+        .map((key) => {
+            if (key in db) {
+                res.push({"len": key.length, "content": db[key]});
+            }
+        });
+    })
+    res.sort((a, b) => {
+        return(10 * (b["len"] - a["len"]) + b["content"]["isspc"] - a["content"]["isspc"]);
+    })
+    const result = res.length > 0 ? res[0]["content"] : {};
+    return JSON.stringify(result);
 }
 
 async function handleRequest(request) {
@@ -31,7 +33,7 @@ async function handleRequest(request) {
     };
     const { searchParams } = new URL(request.url);
     let barcode = searchParams.get("barcode");
-    const results = await gatherResponse(allow, block, barcode);
+    const results = await gatherResponse(barcode);
     return new Response(results, init_json);
     // return new Response(allow, init);
 }
