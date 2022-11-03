7282b0377b9252d092f75283ad82d6cdb0ce42ff
Saengwon Kim <saengwon.kim@gmail.com>
2022-11-03 11:02:55 +0900

    use NanumSquare font

--- frontend/src/html.js
+++ frontend/src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@2.0/nanumsquare.css"></link>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
